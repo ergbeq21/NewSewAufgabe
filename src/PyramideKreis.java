7610112628848a9eddbc48455a95e3fd2717d2b7
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-02-27 11:08:31 +0100

    commit

--- src/PyramideKreis.java
+++ src/PyramideKreis.java
@@ -1,3 +1,16 @@
 public class PyramideKreis extends Pyramide{
+    private double radius;
+    private double pie;
+    public PyramideKreis(double hoehe,double radius, double pie){
+        super(hoehe);
+        this.radius = radius;
+        this.pie = pie;
+    }
+
+    @Override
+    public double volumen() {
+        return (((radius*radius*pie))*getHoehe())/3;
+    }
+
 
 }

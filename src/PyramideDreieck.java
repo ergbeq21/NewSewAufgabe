619a3ee92fb4a6c6e109ca1dfcd65816e7039da2
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2025-02-27 10:56:38 +0100

    New commit

---
+++ src/PyramideDreieck.java
@@ -0,0 +1,12 @@
+public class PyramideDreieck extends Pyramide{
+    private double basis;
+    public PyramideDreieck(double hoehe, double basis) {
+        super(hoehe);
+        this.basis = basis;
+    }
+
+    @Override
+    public double volumen() {
+        return (getHoehe()*basis)/2;
+    }
+}
